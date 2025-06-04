1d66e49fe8119ef3156422918f6e15b6e32bef53
yashlamba <yashlamba2000@gmail.com>
2025-06-04 11:37:19 +0200

    service: components: add RequestReviewersComponent and timeline event

--- invenio_requests/services/requests/components.py
+++ invenio_requests/services/requests/components.py
@@ -9,10 +9,15 @@
 
 """Component for creating request numbers."""
 
+from flask import current_app
+from invenio_i18n import _
 from invenio_records_resources.services.records.components import (
     DataComponent,
     ServiceComponent,
 )
+
+from invenio_requests.customizations.event_types import LogEventType
+from invenio_requests.proxies import current_events_service
 
 
 class RequestNumberComponent(ServiceComponent):
@@ -41,11 +46,42 @@ class RequestDataComponent(DataComponent):
         if record.status == "created":
             keys = ("title", "description", "payload", "receiver", "topic")
         else:
-            keys = ("title", "description", "reviewers")
+            keys = ("title", "description")
 
         for k in keys:
             if k in data:
                 record[k] = data[k]
+
+
+class RequestReviewersComponent(ServiceComponent):
+    """Component for handling request reviewers."""
+
+    def update(self, identity, data=None, record=None, **kwargs):
+        """Update the reviewers of a request."""
+        if "reviewers" in data:
+            if current_app.config.get("REQUESTS_REVIEWERS_ENABLED") is False:
+                raise ValueError(_("Reviewers are not enabled for this request type."))
+            if current_app.config.get("REQUESTS_ENABLE_GROUP_REVIEWERS") is False:
+                # Ensure that reviewers are not groups
+                for reviewer in data["reviewers"]:
+                    if "group" in reviewer:
+                        raise ValueError(_("Group reviewers are not enabled."))
+
+            # Update the reviewers list
+            record["reviewers"] = data["reviewers"]
+
+            # Create an event for the change
+            # TODO Add event based on reviewers added/removed
+            event = LogEventType(
+                payload=dict(
+                    event="reviewers_changed",
+                    content=_("updated reviewers"),
+                )
+            )
+            _data = dict(payload=event.payload)
+            current_events_service.create(
+                identity, record.id, _data, event, uow=self.uow
+            )
 
 
 class RequestPayloadComponent(DataComponent):
