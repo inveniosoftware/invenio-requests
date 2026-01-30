c68a83deb0fd0ab488937081405a458775d3ba87
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-01-31 00:07:57 +0100

    fix(compatibility): TypeError
    * 'staticmethod' object is not callable in python3.9
    
    * but callable in >=python3.10

--- tests/mock_module/request_type.py
+++ tests/mock_module/request_type.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2026 Northwestern University.
+# Copyright (C) 2026 Graz University of Technology.
 #
 # Invenio-Requests is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -12,26 +13,26 @@ from invenio_records_resources.services import EndpointLink
 from invenio_requests.customizations import RequestType
 
 
+def anchor_func(obj, vars):
+    """Anchor generating function.
+
+    This only generates an anchor if request_event is not None in vars.
+    This way the same EndpointLink can be used to generate
+    - the link to the Request UI (no anchor)
+    - the link to the RequestEvent UI (anchor)
+    """
+    request_event = vars["request_event"]
+    if request_event is None:
+        return None
+    return f"commentevent-{request_event.id}"
+
+
 class FakeRequestType(RequestType):
     """A fake RequestType.
 
     There is no such thing as a generic RequestType in reality. This way
     we an also test link mechanism.
     """
-
-    @staticmethod
-    def anchor_func(obj, vars):
-        """Anchor generating function.
-
-        This only generates an anchor if request_event is not None in vars.
-        This way the same EndpointLink can be used to generate
-        - the link to the Request UI (no anchor)
-        - the link to the RequestEvent UI (anchor)
-        """
-        request_event = vars["request_event"]
-        if request_event is None:
-            return None
-        return f"commentevent-{request_event.id}"
 
     links_item = {
         "self_html": EndpointLink(
