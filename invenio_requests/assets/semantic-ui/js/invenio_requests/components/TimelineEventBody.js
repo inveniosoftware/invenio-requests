inveniosoftware/invenio-requests

commit 446689b731d6e5addae7a21cc22d279a78947876

fix: disable comment button when message is empty

invenio_requests/assets/semantic-ui/js/invenio_requests/timelineCommentEditor/TimelineCommentEditor.js
@@ -97,8 +97,11 @@ const TimelineCommentEditor = ({
           size="medium"
           content={saveButtonLabel}
           loading={isLoading}
-          onClick={() => submitComment(commentContent, "html")}
+          onClick={() =>
+            commentContent.length > 0 && submitComment(commentContent, "html")
+          }
           disabled={!canCreateComment}
+          aria-disabled={commentContent.length > 0}
         />
       </div>
     </div>

commit 7c843bfd394ea2d2a886c56fb272644a71ce6bd2

fix(timeline): prevent infinite re-rendering of comment editor
* Due to an effect depending on two functions that are dynamically
re-created by mapStateToDispatch on every state change, the comment
editor was repeatedly calling the 'restore draft' action. This caused
the `initialValue` of the `RichEditor` to get reinitialised, which
'reset' the state of the editor and moved the cursor to the start.

* Since localStorage is synchronous and instant, this is unlikely to
have caused an inconsistency or lost content. But it annoyingly moved
the cursor to the start of the comment every time the auto-refresh
happened.

* This has been fixed by removing the dependencies and disabling eslint
for that line.

* Additionally, a small boolean typo introduced in
https://github.com/inveniosoftware/invenio-requests/pull/553 was fixed.

invenio_requests/assets/semantic-ui/js/invenio_requests/components/TimelineEventBody.js
@@ -97,18 +97,19 @@ const TimelineEventBodyRender = React.forwardRef(
 TimelineEventBodyRender.displayName = "TimelineEventBodyRender";
 
 TimelineEventBodyRender.propTypes = {
-  refInner: PropTypes.instanceOf(Element).isRequired,
+  refInner: PropTypes.object.isRequired,
   isOverflowing: PropTypes.bool.isRequired,
   expanded: PropTypes.bool.isRequired,
   collapsible: PropTypes.bool.isRequired,
   toggleCollapsed: PropTypes.func.isRequired,
   content: PropTypes.string.isRequired,
   format: PropTypes.string,
-  files: PropTypes.array.isRequired,
+  files: PropTypes.array,
 };
 
 TimelineEventBodyRender.defaultProps = {
   format: null,
+  files: [],
 };
 
 const TimelineEventBodyContainer = ({

invenio_requests/assets/semantic-ui/js/invenio_requests/timeline/TimelineFeed.js
@@ -202,7 +202,7 @@ class TimelineFeed extends Component {
                   }
                   saveButtonIcon={isReplyTimeline ? "reply" : "send"}
                   onCancel={isReplyTimeline ? this.onCancelClick : undefined}
-                  request={request}
+                  requestId={request.id}
                 />
               )}
 

invenio_requests/assets/semantic-ui/js/invenio_requests/timelineCommentEditor/TimelineCommentEditor.js
@@ -32,15 +32,20 @@ const TimelineCommentEditor = ({
   saveButtonIcon,
   onCancel,
   disabled,
-  request,
+  requestId,
 }) => {
   useEffect(() => {
     restoreCommentContent();
-  }, [restoreCommentContent]);
-
-  useEffect(() => {
     restoreCommentFiles();
-  }, [restoreCommentFiles]);
+    // These functions are re-created on every top-level render of the timeline feed both for parents and replies.
+    // For example, whenever the Redux state changes, the functions are recreated.
+    // This therefore causes the restore to happen repeatedly on every render, which might lead to race conditions
+    // or unexpected UI behaviour.
+    // The functions will not actually change in a meaningful way. The only way this change would be needed is if the
+    // ID of the parent request event (for a reply timeline) changes, which is not possible.
+    // Therefore it is reasonable to not include them as effect dependencies.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const editorRef = useRef(null);
   useEffect(() => {
@@ -63,12 +68,12 @@ const TimelineCommentEditor = ({
 
   const onFileUpload = async (filename, payload, options) => {
     const client = new InvenioRequestFilesApi();
-    return await client.uploadFile(request.id, filename, payload, options);
+    return await client.uploadFile(requestId, filename, payload, options);
   };
 
   const onFileDelete = async (file) => {
     const client = new InvenioRequestFilesApi();
-    await client.deleteFile(request.id, file.key);
+    await client.deleteFile(requestId, file.key);
   };
 
   return (
@@ -126,7 +131,7 @@ const TimelineCommentEditor = ({
             commentContent.length > 0 && submitComment(commentContent, "html", files)
           }
           disabled={!canCreateComment}
-          aria-disabled={commentContent.length > 0}
+          aria-disabled={commentContent.length === 0}
         />
       </div>
     </div>
@@ -152,7 +157,7 @@ TimelineCommentEditor.propTypes = {
   saveButtonIcon: PropTypes.string,
   onCancel: PropTypes.func,
   disabled: PropTypes.bool,
-  request: PropTypes.object.isRequired,
+  requestId: PropTypes.string.isRequired,
 };
 
 TimelineCommentEditor.defaultProps = {