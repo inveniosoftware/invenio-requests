f149251c31fbcf383f2c904d269af8582895b4a8
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2026-04-01 14:57:28 +0200

    ui(translations): statically register available request actions so they can be picked up by i18next

--- invenio_requests/assets/semantic-ui/js/invenio_requests/request/actions/RequestActions.js
+++ invenio_requests/assets/semantic-ui/js/invenio_requests/request/actions/RequestActions.js
@@ -12,8 +12,18 @@ import { RequestAction } from "./RequestAction";
 import { Dropdown } from "semantic-ui-react";
 import { AppMedia } from "@js/invenio_theme/Media";
 import PropTypes from "prop-types";
+import { i18next } from "../../../../translations/invenio_requests/i18next";
 
 export const RequestActions = ({ request, size }) => {
+  const available_actions = [
+    i18next.t("create"),
+    i18next.t("submit"),
+    i18next.t("delete"),
+    i18next.t("accept"),
+    i18next.t("decline"),
+    i18next.t("cancel"),
+    i18next.t("expire"),
+  ];
   const actions = Object.keys(new RequestLinksExtractor(request).actions);
   const { MediaContextProvider, Media } = AppMedia;
 
